919a79c12ba22f1cde4f8acc036925c57ade81ae
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-10-24 07:41:17 -0300

    Iniciando projeto

---
+++ projeto-1/app.js
@@ -0,0 +1,18 @@
+new Vue({
+  el: "#matadorMonstros",
+  data: {
+    jogoIniciado: false,
+    personagens: [
+      {
+        id: "J1",
+        nome: "Jogador",
+        vida: 100
+      },
+      {
+        id: "J2",
+        nome: "Monstro",
+        vida: 100
+      },
+    ]
+  }
+});
