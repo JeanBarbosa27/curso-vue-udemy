654d84d470380f7db69e54874319e1a49d8a1769
Jean <jeanbarbosa9@gmail.com>
2019-11-04 18:33:40 -0300

    Exemplo da aula sobre arquivos .vue

--- vue-cli/projeto-simples/src/App.vue
+++ vue-cli/projeto-simples/src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ titulo }}</h1>
+    <button @click="titulo += '#'">Alterar</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      titulo: "Testa Data usando Vue JS!!"
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #ffffff;
+  background-color: chocolate;
 }
 </style>
