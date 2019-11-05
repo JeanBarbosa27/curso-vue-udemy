c5a18cea804aa2bf69b41f304444945c1ceb7737
Jean <jeanbarbosa9@gmail.com>
2019-11-05 17:10:32 -0300

    Arrumando estrutura de pastas

--- componentes/componente-desafio/src/components/widgets/Carousel.vue
+++ componentes/componente-desafio/src/components/widgets/Carousel.vue
@@ -1,12 +1,11 @@
 <template>
-  <v-content>
-			<v-carousel>
-				<v-carousel-item
-					v-for="(item,i) in fotos"
-					:key="i"
-					:src="item.src"></v-carousel-item>
-			</v-carousel>
-		</v-content>
+  <v-carousel>
+    <v-carousel-item
+        v-for="(item,i) in fotos"
+        :key="i"
+        :src="item.src"></v-carousel-item>
+  </v-carousel>
+
 </template>
 
 <script>
---
+++ componentes/componente-desafio/src/main/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<v-app>
+		<main-menu />
+		<toolbar />
+		<v-content>
+			<carousel />
+		</v-content>
+		<main-footer />
+	</v-app>
+</template>
+
+<script>
+	import menu from "@/components/template/Menu";
+	import toolbar from "@/components/template/ToolBar";
+	import carousel from "@/components/widgets/Carousel";
+	import footer from "@/components/template/Footer";
+export default {
+	components: {
+		"main-menu": menu,
+		toolbar,
+		carousel,
+		"main-footer": footer
+	}
+}
+</script>
