1ee1df08832f5970b69f6765c1843e2e6154772d
Jean <jeanbarbosa9@gmail.com>
2019-11-06 13:09:18 -0300

    Opções de props em componentes

--- componentes/componentes-comunicacao/src/components/UsuarioInfo.vue
+++ componentes/componentes-comunicacao/src/components/UsuarioInfo.vue
@@ -2,15 +2,24 @@
     <div class="componente">
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
-        <p>Nome do usuário: <strong>{{ nome }}</strong></p>
+        <p>Nome do usuário: <strong>{{ inverterNome() }}</strong></p>
     </div>
 </template>
 
 <script>
 export default {
     props:  {
-            nome: String
+        nome: {
+            type: String,
+            //required: true,
+            default: "Anônimo"
         }
+    },
+    methods: {
+        inverterNome() {
+            return this.nome.split("").reverse().join("");
+        }
+    }
 }
 </script>
 
