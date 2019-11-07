59a5c7b038726c949e47d1e1a56477fdb8ca1228
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-11-07 16:23:36 -0300

    Versao dinamica a partir do package json

--- componentes-avancados/citacoes/src/components/Sobre.vue
+++ componentes-avancados/citacoes/src/components/Sobre.vue
@@ -1,38 +1,43 @@
 <template>
-    <div>
-        <p class="nome">Citações</p>
-        <p class="versao">Versão 1.0</p>
-    </div>
+  <div>
+    <p class="nome">Citações</p>
+    <p class="versao">Versão {{ versao }}</p>
+  </div>
 </template>
 
 <script>
+import { version } from "../../package.json";
 export default {
-
-}
+  data() {
+    return {
+      versao: version
+    };
+  }
+};
 </script>
 
 <style scoped>
-    div {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        border: 1px solid #DDD;
-        height: 200px;
-        width: 200px;
-        border-radius: 100px;
-    }
+div {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #ddd;
+  height: 200px;
+  width: 200px;
+  border-radius: 100px;
+}
 
-    p {
-        margin: 0;
-    }
+p {
+  margin: 0;
+}
 
-    .nome {
-        font-size: 2.1rem;
-        font-weight: bold;
-    }
+.nome {
+  font-size: 2.1rem;
+  font-weight: bold;
+}
 
-    .versao {
-        font-size: 1.4rem;
-    }
+.versao {
+  font-size: 1.4rem;
+}
 </style>
