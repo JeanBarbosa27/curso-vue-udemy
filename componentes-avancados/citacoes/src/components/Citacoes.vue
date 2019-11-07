987d3d3c99bb46f0a37c45094bfda174f346ef44
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-11-07 15:58:43 -0300

    Usando multiplos slots nomeados

--- componentes-avancados/citacoes/src/components/Citacoes.vue
+++ componentes-avancados/citacoes/src/components/Citacoes.vue
@@ -6,9 +6,9 @@
     </span>
     <p>{{ numero }} {{ indice }}</p>
     <citacao>
-      <h1>{{ citacoes[indice].autor }}</h1>
-      <p>{{ citacoes[indice].texto }}</p>
-      <h6>{{ citacoes[indice].texto }}</h6>
+      <h6 slot="fonte">{{ citacoes[indice].fonte }}</h6>
+      <p slot="texto">{{ citacoes[indice].texto }}</p>
+      <h1 slot="autor">{{ citacoes[indice].autor }}</h1>
     </citacao>
   </div>
 </template>
