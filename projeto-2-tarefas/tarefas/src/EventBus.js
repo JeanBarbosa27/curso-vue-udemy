91e3f9983de3d089748691f13e86a7e60125c708
Jean <jeanbarbosa9@gmail.com>
2019-11-08 11:07:31 -0300

    Mock de state e criação e edição de métodos

--- projeto-2-tarefas/tarefas/src/EventBus.js
+++ projeto-2-tarefas/tarefas/src/EventBus.js
@@ -2,7 +2,17 @@ import Vue from "vue";
 export default new Vue({
   data() {
     return {
-      tasks: []
+      tasks: [
+        {
+          text: "bla bla",
+          status: "done"
+        },
+        {
+          text: "ble ble",
+          status: "pending"
+        },
+
+      ]
     };
   },
   methods: {
@@ -11,6 +21,9 @@ export default new Vue({
     },
     getTask(taskId) {
       return this.tasks.filter(task => task.id === taskId);
+    },
+    setTasks(tasks) {
+      return this.tasks = tasks;
     },
     getLocalTasks() {
       if (localStorage.todoVueUdemy) {
@@ -25,14 +38,20 @@ export default new Vue({
       this.$emit("taskAdded", task);
     },
     onTaskAdded(callback) {
-      this.$on("taskAdded", callback);
+      this.$on("taskAdded", () => {
+        this.saveLocalTasks();
+        callback();
+      });
     },
     removeTask(taskId) {
       this.tasks = this.tasks.filter(task => task.id !== taskId);
       this.$emit("taskRemoved");
     },
     onTaskRemoved(callback) {
-      this.$on("taskRemoved", callback);
+      this.$on("taskRemoved", () => {
+        this.saveLocalTasks();
+        callback();
+      });
     }
   }
 });
