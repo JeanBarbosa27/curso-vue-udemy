f59291299bc658ff892160c247451c577dcaa7d5
Jean <jeanbarbosa9@gmail.com>
2019-11-08 16:52:16 -0300

    Construção completa do componente

--- projeto-2-tarefas/tarefas/src/components/TasksList.vue
+++ projeto-2-tarefas/tarefas/src/components/TasksList.vue
@@ -1,10 +1,43 @@
 <template>
-  <div class="tasks-list"></div>
+  <ul class="tasks-list">
+    <li v-for="task in this.$eventBus.tasks" :key="task.text" class="tasks-list__item">
+      <task-item :text="task.text" :status="task.status" />
+    </li>
+  </ul>
 </template>
 <script>
+  import taskItem from "@/components/TaskItem";
 export default {
-  name: "TasksList"
+  name: "TasksList",
+  components: {
+    taskItem
+  }
 };
 </script>
 <style>
+
+  .tasks-list {
+    display: flex;
+    width: 80%;
+    margin: 0;
+    padding: 0;
+    justify-content: center;
+    flex-wrap: wrap;
+    list-style: none;
+
+  }
+  .tasks-list__item {
+    width: 100%;
+    margin-right: 10px;
+    margin-bottom: 10px;
+  }
+
+  @media only screen and (min-width: 1025px) {
+    .tasks-list {
+      justify-content: flex-start;
+    }
+    .tasks-list__item {
+      max-width: 200px;
+    }
+  }
 </style>
