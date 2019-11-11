8d5eac29caf7e6fadf8f06d2ab83f9ca68333897
Jean <jeanbarbosa9@gmail.com>
2019-11-11 17:48:30 -0300

    Download, instalação e inicio: filtros

---
+++ filtros-mixins/filtros-mixins-exercicios/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+	<div id="app">
+		<h1>Filtros & Mixins</h1>
+		<hr>
+		<p>{{ cpf | cpf | inverter }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			cpf: "12345678900"
+		}
+	},
+	filters: {
+		cpf(valor) {
+			const cpf = `${valor}`.split("")
+			cpf.splice(3,0,".")
+			cpf.splice(7,0,".")
+			cpf.splice(11,0,"-")
+			return cpf.join("")
+		}
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+</style>
