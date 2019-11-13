85198bd624fd2d2eae0cff526de054e948c0ac00
Jean <jeanbarbosa9@gmail.com>
2019-11-13 18:13:46 -0300

    Appear em elementos que renderizam por padrão

--- animacoes/animacoes-exercicios/src/App.vue
+++ animacoes/animacoes-exercicios/src/App.vue
@@ -1,74 +1,79 @@
 <template>
-	<div id="app" class="container-fluid">
-		<h1>Animações</h1>
-		<hr>
-		<b-button variant="primary" @click="exibir = !exibir" class="mb-4">Mostrar mensagem</b-button>
-		<transition name="fade">
-			<b-alert variant="success" show v-if="exibir">Mensagem exibida via animação</b-alert>
-		</transition>
-		<transition name="slide">
-			<b-alert variant="success" show v-if="exibir" type="transition">Mensagem exibida via animação</b-alert>
-		</transition>
-
-	</div>
+  <div id="app" class="container-fluid">
+    <h1>Animações</h1>
+    <hr>
+    <transition name="fade" appear>
+      <b-button variant="primary" @click="exibir = !exibir" class="mb-4">Mostrar mensagem</b-button>
+    </transition>
+    <transition name="fade">
+      <b-alert variant="success" show v-if="exibir">Mensagem exibida via animação</b-alert>
+    </transition>
+    <transition name="slide">
+      <b-alert variant="success" show v-if="exibir" type="transition">Mensagem exibida via animação</b-alert>
+    </transition>
+  </div>
 </template>
 
 <script>
 
-export default {
-	data() {
-		return {
-			exibir: false,
-		}
-	}
-}
+  export default {
+    data() {
+      return {
+        exibir: false,
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-	font-size: 1.5rem;
-}
-	.fade-enter, .fade-leave-to {
-		opacity: 0;
-	}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    font-size: 1.5rem;
+  }
 
-	.fade-enter-active, .fade-leave-active {
-		transition: opacity 1s;
-	}
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
 
-	@keyframes slide-in {
-		from {
-			transform: translateX(-100%);
-		}
-		to {
-			transform: translateX(0);
-		}
-	}
-	@keyframes slide-out {
-		from {
-			transform: translateX(0);
-		}
-		to {
-			transform: translateX(100%);
-		}
-	}
-	.slide-enter-active {
-		animation: slide-in 1s ease;
-		transition: opacity 2s;
-	}
-	.slide-leave-active {
-		animation: slide-out 1s ease;
-		transition: opacity 0.5s;
-	}
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 1s;
+  }
 
-	.slide-enter, .slide-leave-to {
-		opacity: 0;
-	}
+  @keyframes slide-in {
+    from {
+      transform: translateX(-100%);
+    }
+    to {
+      transform: translateX(0);
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateX(0);
+    }
+    to {
+      transform: translateX(100%);
+    }
+  }
+
+  .slide-enter-active {
+    animation: slide-in 1s ease;
+    transition: opacity 2s;
+  }
+
+  .slide-leave-active {
+    animation: slide-out 1s ease;
+    transition: opacity 0.5s;
+  }
+
+  .slide-enter, .slide-leave-to {
+    opacity: 0;
+  }
 
 </style>
