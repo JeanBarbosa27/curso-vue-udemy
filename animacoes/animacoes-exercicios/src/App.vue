b82588e7b79f4a49492433e382793e75f436ec6a
Jean <jeanbarbosa9@gmail.com>
2019-11-13 16:55:46 -0300

    Exemplo de transição com as classes do Vue

--- animacoes/animacoes-exercicios/src/App.vue
+++ animacoes/animacoes-exercicios/src/App.vue
@@ -1,13 +1,22 @@
 <template>
 	<div id="app" class="container-fluid">
 		<h1>Animações</h1>
+		<hr>
+		<b-button variant="primary" @click="exibir = !exibir" class="mb-4">Mostrar mensagem</b-button>
+		<transition name="fade">
+			<b-alert variant="success" show v-if="exibir">Mensagem exibida via animação</b-alert>
+		</transition>
 	</div>
 </template>
 
 <script>
 
 export default {
-
+	data() {
+		return {
+			exibir: false,
+		}
+	}
 }
 </script>
 
@@ -21,4 +30,11 @@ export default {
 	margin-top: 60px;
 	font-size: 1.5rem;
 }
+	.fade-enter, .fade-leave-to {
+		opacity: 0;
+	}
+
+	.fade-enter-active, .fade-leave-active {
+		transition: opacity 1s;
+	}
 </style>
