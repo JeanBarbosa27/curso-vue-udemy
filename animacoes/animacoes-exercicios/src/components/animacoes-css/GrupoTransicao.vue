b0fa1c59b894261ba5b6349ef27842e9e78fceb4
Jean <jeanbarbosa9@gmail.com>
2019-11-14 13:40:41 -0300

    Adicionando estrutura para transição em grupo

--- animacoes/animacoes-exercicios/src/components/animacoes-css/GrupoTransicao.vue
+++ animacoes/animacoes-exercicios/src/components/animacoes-css/GrupoTransicao.vue
@@ -1,5 +1,23 @@
 <template>
-  
+  <div class="d-flex row justify-content-center">
+    <span class="col-4">Total de tokes: {{ tokens.length }}</span>
+    <transition-group
+      class="col-4"
+      style="height: 372px; user-select: none; overflow-y: auto"
+    >
+      <b-list-group
+        v-for="(token, i) in tokens"
+        :key="i"
+      >
+        <b-list-group-item style="cursor: pointer" @click="removerToken(i)">
+          {{ token }}
+        </b-list-group-item>
+      </b-list-group>
+    </transition-group>
+    <b-button variant="primary" style="height: 50px" class="col-2" @click="adicionarToken">
+      Adicionar token
+    </b-button>
+  </div>
 </template>
 
 <script>
@@ -11,7 +29,7 @@
       }
     },
     methods: {
-      adicionarTokens() {
+      adicionarToken() {
         const token = Math.random().toString(36).substring(2);
         this.tokens.push(token);
       },
