9af9f749f46ab2dac02add283f62934784dfcb28
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-01-15 18:59:38 -0300

    Criando o form e enviando dados para o firebase

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,55 @@
 <template>
 	<div id="app" class="container">
 		<h1>HTTP com Axios</h1>
+		<b-card>
+			<b-form-group labe="Nome:">
+				<b-form-input
+					type="text"
+					size="lg"
+					v-model="usuario.nome"
+					placeholder="Informe o nome"
+				></b-form-input>
+			</b-form-group>
+			<b-form-group labe="E-mail:">
+				<b-form-input
+					type="text"
+					size="lg"
+					v-model="usuario.email"
+					placeholder="Informe o e-mail"
+				></b-form-input>
+			</b-form-group>
+			<hr>
+			<b-button
+				size="lg"
+				variant="primary"
+				@click="salvar"
+			>
+				Salvar
+			</b-button>
+		</b-card>
 	</div>
 </template>
 
 <script>
 export default {
-
+	data() {
+		return {
+			usuario: {
+				nome: "",
+				email: ""
+			}
+		}
+	},
+	methods: {
+		salvar() {
+			this.$http.post("usuarios.json", this.usuario).then(
+				res => {
+					this.usuario.nome = "";
+					this.usuario.email = "";
+				}
+			)
+		}
+	}
 }
 </script>
 
