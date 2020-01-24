f5e386c547eea159392bfd1ae767ef2c4aa14264
“Jean <jbarbosa@estantevirtual.com.br>
2020-01-24 17:06:36 -0300

    Interceptando solicitções

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -6,5 +6,13 @@ axios.defaults.baseURL = "https://curso-vue-udemy-e262c.firebaseio.com/";
 Vue.use({
   install(Vue) {
     Vue.prototype.$http = axios;
+
+    Vue.prototype.$http.interceptors.request.use(config => {
+      // if(config.method == "post") {
+      //   config.method = "put";
+      // }
+      console.log(config.method);
+      return config;
+    })
   }
 });
