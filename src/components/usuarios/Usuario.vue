7894860d05b27f8af899cd0f3a00823c70da9c1b
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-02-12 22:42:03 -0300

    Lendo parâmetros da rota

--- src/components/usuarios/Usuario.vue
+++ src/components/usuarios/Usuario.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
     <h2>Usuario</h2>
+    <hr/>
+    <p><strong>Código: {{ id }} </strong></p>
+    <button sucesso @click="irParaInicio">Voltar</button>
   </div>
 </template>
 
 <script>
 export default {
-  
+  name: "Usuario",
+  data() {
+    return {
+      id: this.$route.params.id
+    }
+  },
+  watch: {
+    $route(to, from) {
+      this.id = to.params.id
+    }
+  },
+  methods: {
+    irParaInicio() {
+      this.$router.push({ path: "/" })
+    }
+  }
 }
 </script>
 <style>
