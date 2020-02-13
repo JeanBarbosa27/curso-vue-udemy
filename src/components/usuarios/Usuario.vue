05e95ed83cd5b9940e8ed999bedc7cc3586a6ea2
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-02-12 22:47:37 -0300

    Lendo parâmetros da rota através de propriedades

--- src/components/usuarios/Usuario.vue
+++ src/components/usuarios/Usuario.vue
@@ -3,23 +3,25 @@
     <h2>Usuario</h2>
     <hr/>
     <p><strong>Código: {{ id }} </strong></p>
-    <button sucesso @click="irParaInicio">Voltar</button>
+    <button sucesso @click="irParaInicio">Voltar para início</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "Usuario",
-  data() {
-    return {
-      id: this.$route.params.id
-    }
-  },
-  watch: {
-    $route(to, from) {
-      this.id = to.params.id
-    }
-  },
+  props: ["id"],
+  // Essa forma abaixo é a forma antiga e mais verbosa de fazer essa leitura dos parâmetros. A forma mais simples e mais recente suportada pelo vue router é através de props, que precisa ter o mesmo nome que for definido na configuração da rota.
+  // data() {
+  //   return {
+  //     id: this.$route.params.id
+  //   }
+  // },
+  // watch: {
+  //   $route(to, from) {
+  //     this.id = to.params.id
+  //   }
+  // },
   methods: {
     irParaInicio() {
       this.$router.push({ path: "/" })
