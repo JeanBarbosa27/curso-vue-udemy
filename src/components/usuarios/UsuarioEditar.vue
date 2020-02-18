742c423618ac687cd230e488b5b1c310c9f80852
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-02-18 06:52:17 -0300

    Interceptando a navegação antes desta ocorrer

--- src/components/usuarios/UsuarioEditar.vue
+++ src/components/usuarios/UsuarioEditar.vue
@@ -8,7 +8,18 @@
 <script>
 export default {
   name: "UsuaioEditar",
-  props: ["id"]
+  props: ["id"],
+  beforeRouteEnter(to, from, next) {
+    console.log("Interceptação de mudança de rota, dentro do componente");
+
+    // É possível também fazer a interceptação antes que a rota seja alterada diretamente pelo componente, porém acho mais interessante que isso seja feito em routes, pois fica mais claro quais rotas tem algum tipo de verificação por autenticação ou por exemplo poderia centralizar a chamada desse "mixin".
+
+    // next(autenticado); // Acho essa forma de permitir ou não a navegação mais sucinta, pois economiza no if (conforme o exemplo da interceptação específica da rota em routes.js), porém com ela não é possível fazer uma ação para caso a autenticação não seja permitida, e visualmente para o usuário não teria nenhuma resposta.
+
+    // Uma outra forma de fazer aqui, sem usar diretamnte o if poderia ser:
+    const autenticado = true;
+    autenticado ? next() : next("/?redirect=editarUsuario"); // De modo que se não tiver autenticado é redirecionado para home ou poderia ser para /login e após o procedimento de login, poderia ser redirecionado de volta para esse componente, mas para isso precisaria de uma tratativa na rota de login, caso recebesse "redirect" como parâmetro de URL.
+  }
 }
 </script>
 
--- src/router.js
+++ src/router.js
@@ -9,7 +9,7 @@ import UsuarioLista from './components/usuarios/UsuarioLista';
 
 Vue.use(Router);
 
-export default new Router({
+const routes = new Router({
   mode: "history",
   scrollBehavior(to, from, savedPosition) {
     if(savedPosition) {
@@ -34,9 +34,39 @@ export default new Router({
       props: true,
       children: [
         { path: '', component: UsuarioLista },
-        { path: ':id', component: UsuarioDetalhe, props: true, name: "detalheUsuario" },
+        { 
+          path: ':id',
+          component: UsuarioDetalhe,
+          props: true,
+          name: "detalheUsuario",
+          beforeEnter(to, from, next) {
+            // Forma de interceptar a navegação dessa rota exclusivamente. Usando o mesmo exemplo da autenticação, poderia ser usado para verificar se o usuário está logado, para permitir a navegação ou não. A assinatura de parâmetros aqui é igual à routes.beforeEach.
+
+            // Suponha que tem uma forma de verificar se o usuário está logado.
+            const autenticado = true;
+            console.log("Interceptação exclusiva da rota de detalhes do usuário, antes de seguir com a navegação");
+            
+            
+            if(autenticado) {
+              console.log("Interceptação exclusiva da rota de detalhes do usuário, antes de seguir com a navegação")
+              next(); // Isso fará com que só seja redirecionado para essa rota caso esteja autenticado;
+            } else {
+              console.log("Retorna para o usuário alguma interação ou realiza outra ação, como redirecionar para o login, para que não fique travado na tela sem saber o que está acontecendo")
+            }
+          }
+        },
         { path: ':id/editar', component: UsuarioEditar, props: true, name: "editarUsuario" },
       ]
     },
   ]
 })
+
+routes.beforeEach((to, from, next) => {
+  // Esse é um método de interceptação global de rotas, de modo que é possível fazer validações, para permitir ou não o prosseguimento da navegação. Um caso de uso interessante seria para o caso de ser necessário estar com algum tipo de autenticação em todas as rotas para poder navegar na aplicação.
+  // to é de para onde está vindo, from é de onde está vindo e next permite seguir em frente ou não.
+
+  console.log("Interceptação global de rotas, antes de iniciar a navegação");
+  next(); // é possível interromper a navegação não colocando o next ou passando false como parãmetro.
+})
+
+export default routes;
