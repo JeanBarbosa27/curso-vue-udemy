7f6f94d8bde0af1cd1cb05a5b31afd590e962e6c
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-02-19 06:50:10 -0300

    Aula 278: Configurando o estado centralizado da aplicação

--- src/components/Carrinho.vue
+++ src/components/Carrinho.vue
@@ -28,18 +28,12 @@
 <script>
 export default {
     computed: {
+        produtos() {
+            return this.$store.state.produtos;
+        },
         total() {
             return this.produtos.map(p => p.quantidade * p.preco)
                 .reduce((total, atual) => total + atual, 0)
-        }
-    },
-    data() {
-        return {
-            produtos: [
-                { id: 1, nome: 'Produto 1', quantidade: 7, preco: 14.55 },
-                { id: 2, nome: 'Produto 2', quantidade: 10, preco: 22.99 },
-                { id: 3, nome: 'Produto 3', quantidade: 1, preco: 43.18 },
-            ]
         }
     }
 }
