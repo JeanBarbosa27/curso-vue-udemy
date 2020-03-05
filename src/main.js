57cf731baa3cbd4469ff7af2057b0919969838bd
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-03-05 00:24:01 -0300

    Importando os plugins e a store

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import store from './store'
 import router from './router'
+
+import './plugins/vuetify'
+import './plugins/axios'
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   router,
 	render: h => h(App),
 }).$mount('#app')
