92974fe30515e1314757b61d977f09fee1418817
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-03-05 00:22:45 -0300

    Configurações globais do axios

---
+++ src/plugins/axios.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import axios from 'axios';
+
+Vue.use({
+  install(Vue) {
+    Vue.prototype.$http = axios.create {
+      baseURL: 'inserir-url-do-projeto-no-firebase',
+    }
+
+    Vue.prototype.$http.interceptors.response.use(response => {
+      const array = [];
+      for(let key in res.data) {
+        array.push({ id: key, ...res.data[key] });
+      }
+
+      res.data = array;
+      return res
+    }, error => Promise.reject(error));
+  }
+});
+
+
