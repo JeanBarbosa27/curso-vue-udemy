141224b4fa42ae20044a52f4d32c0d602a69772c
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-03-06 23:33:40 -0300

    Iniciando a home

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="home">
+    <h1 class="home__title">Negocie e consulte suas Ações</h1>
+    <div class="home__content">
+      <v-alert type="info">Você pode Salvar & Carregar os Dados</v-alert>
+      <v-alert type="info" color="success">Clique em 'Finalizar dia' para iniciar um novo dia!</v-alert>
+    </div>
+  </div>
+</template>
