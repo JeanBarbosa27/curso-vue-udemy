1c7e78a6fe55ed54553c1d527e4ace12c413531b
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-03-07 23:34:00 -0300

    Finalizando a home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="home">
-    <h1 class="home__title">Negocie e consulte suas Ações</h1>
+    <h1 class="home__title">Negocie e Consulte suas Ações</h1>
     <v-app class="home__content">
       <v-alert class="home__alert" :value="true"  type="info">Você pode Salvar & Carregar os Dados</v-alert>
       <v-alert class="home__alert" :value="true" type="info" color="success">Clique em 'Finalizar dia' para iniciar um novo dia!</v-alert>
-      <v-divider />
-    <h3 class="home__subtitle">Seu saldo: R$ {{ getBalance }}</h3>
+      <v-divider class="home__divider" />
+    <h3 class="home__subtitle">Seu Saldo: {{ saldo | formatarMoeda }}</h3>
     </v-app>
   </div>
 </template>
@@ -34,14 +34,26 @@
 
   .home__title {
     margin-bottom: 15px;
+    font-size: 2.5em;
+    color: #333333;
+    font-weight: 300;
   }
 
   .home__alert {
     width: 100%;
-    margin-bottom: 15px;
+    margin: 0;
+    margin-bottom: 10px;
     padding: 10px;
     border: 0;
-    box-shadow: 0 2px 3px rgba(0,0,0,0.5);
+    box-shadow: 0 3px 6px rgba(0,0,0,0.3);
   }
 
+  .home__divider {
+    margin: 10px 0;
+  }
+
+  .home__subtitle {
+    margin: 10px 0;
+    font-size: 1.5em;
+  }
 </style>
