8da9f1a729877492c1862c61a3a1621ce411c291
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-03-08 00:00:21 -0300

    Inciciando criação do header

---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+    <div class="logo">
+      <strong>stock</strong><span>trader</span>
+    </div>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+  }
+</script>
+
+<style>
+  .header {
+    
+  }
+
+  .logo {
+    font-size: 1.5em;
+    text-transform: uppercase;
+  }
+
+</style>
