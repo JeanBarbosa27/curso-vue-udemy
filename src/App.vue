83ff71484006f23283b51df007bb4f0aecd26843
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-03-08 00:00:32 -0300

    Importando e renderizando header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div id="app" class="stock-trader">
-    <h1>Stock Trader</h1>
+   <Header />
     <router-view />
   </div>
 </template>
 
 <script>
 
+import Header from './components/layouts/Header.vue';
+
 export default {
   name: 'App',
+  components: {
+    Header,
+  }
 }
 </script>
 
