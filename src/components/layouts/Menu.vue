c12c44f904d47ca1b05ee3f020cf17d9ed565159
Jean Barbosa <jbarbosa@estantevirtual.com.br>
2020-03-09 13:35:24 -0300

    Iniciando a contrução do menu

---
+++ src/components/layouts/Menu.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="menu">
+    <ul class="menu__list">
+      <router-link
+        v-for="(item, index) in items"
+        :key="index"
+        :to="item.path"
+        class="menu__item"
+        tag="li"
+      >{{ item.text }}</router-link>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'Menu',
+    data() {
+      return {
+        items: [
+          {
+            path: '/stocks',
+            text: 'Stocks'
+          },
+          {
+            path: '/portfolio',
+            text: 'Portfolio'
+          },
+        ]
+      }
+    }
+  }
+</script>
+<style lang="scss">
+.menu {
+  &__list {
+    display: flex;
+    list-style: none;
+  }
+}
+</style>
