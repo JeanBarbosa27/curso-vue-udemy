810c240837e748d55329d21230edeb4db8b3ba32
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-03-09 00:14:56 -0300

    Preparando a estrutura do header

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,25 +1,52 @@
 <template>
-  <header class="header">
+  <v-app-bar
+    class="header"
+    :dense="true"
+    :short="true"
+    height="50px"
+  >
     <div class="logo">
       <strong>stock</strong><span>trader</span>
     </div>
-  </header>
+    <div class="header__menu">
+      <span>Inserir menu de rotas</span>
+      <span>Inserir menu de ações</span>
+    </div>
+    <span class="header__balance">saldo {{ getBalance | currencyFormat }}</span>
+  </v-app-bar>
 </template>
 
 <script>
+  import { mapGetters } from 'vuex';
   export default {
     name: 'Header',
+    computed: {
+      ...mapGetters(['getBalance']),
+    }
   }
 </script>
 
-<style>
+<style lang="scss">
   .header {
     
+    &__menu {
+      display: flex;
+      flex: 1 1 100%;
+      padding: 0 10px;
+      justify-content: space-between;
+    }
+    
+    &__balance {
+      display: block;
+      white-space: nowrap;
+    }
+
+   .logo {
+      font-size: 1.5em;
+      text-transform: uppercase;
+    }   
   }
 
-  .logo {
-    font-size: 1.5em;
-    text-transform: uppercase;
-  }
+  
 
 </style>
