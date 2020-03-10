88b48079a9adc2ac383ebf49c11eda2699523344
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-03-10 00:17:58 -0300

    Criação do action menu Para finalizar o dia, salvar e carregar dados usando o Firebase

---
+++ src/components/layouts/ActionMenu.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="action-menu">
+    <ul class="action-menu__list">
+      <li class="action-menu__item" @click="finishDay">
+        finalizar dia
+      </li>
+      <li class="action-menu__item">
+        Salvar &#38; Carregar
+        <ul class="action-menu__submenu-list">
+          <li class="action-menu__submenu-item" @click="saveData">
+            Salvar dados
+          </li>
+          <li class="action-menu__submenu-item" @click="loadData">
+            Carregar dados
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ActionMenu',
+  methods: {
+    finishDay() {
+      console.log('Inserir lógica de finalizar o dia');
+    },
+    saveData() {
+      console.log('Inserir requisição para salvar dados no Firebase');
+    },
+    loadData() {
+      console.log('Inserir requisição para carregar dados do Firebase');
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.action-menu {
+  display: flex;
+
+  &__list {
+    display: flex;
+    list-style: none;
+  }
+
+  &__item {
+    position: relative;
+    padding: 17px 10px;
+    font-size: 0.8em;
+    font-weight: 600;
+    text-transform: uppercase;
+    cursor: pointer;
+    transition: background-color 300ms ease-in;
+
+    &.active,
+    &:hover,
+    &:focus {
+      background: #dddddd;
+    }
+
+    &:hover {
+      .action-menu {
+        &__submenu-list {
+          opacity: 1;
+          visibility: visible;
+        }
+
+      }
+      
+    }
+  }
+
+  &__submenu {
+    &-list {
+      position: absolute;
+      top: 100%;
+      left: 50%;
+      width: 100%;
+      background: #ffffff;
+      list-style: none;
+      box-shadow: 0 3px 6px rgba(0,0,0,0.3);
+      transform: translateX(-50%);
+      opacity: 0;
+      visibility: hidden;
+      transition: opacity 300ms ease-in, visibility 300ms ease-in;
+    }
+
+    &-item {
+      padding: 10px;
+      font-size: 0.8em;
+      font-weight: 600;
+      cursor: pointer;
+      transition: background-color 300ms ease-in;
+
+      &:hover,
+      &:focus {
+        background: #dddddd;
+      }
+    }
+  }
+}
+</style>
