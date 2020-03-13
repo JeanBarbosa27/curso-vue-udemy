12ccc25b28c291747e4d2b8916e194947081fb82
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-03-12 23:41:52 -0300

    Corrigindo de onde vem a lista

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -37,7 +37,7 @@ export default {
   computed: {
     ...mapGetters(['getListFrom']),
     portfolio() {
-      return this.getListFrom('stocks');
+      return this.getListFrom('portfolio');
     } 
   },
   methods: {
