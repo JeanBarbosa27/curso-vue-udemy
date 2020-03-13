d67a6f4b684fe65548286f5c506cc8320234d3c1
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-03-12 23:41:36 -0300

    Inserindo transição na navegação

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app" class="stock-trader">
-   <Header />
-    <router-view />
+    <Header />
+    <transition 
+      mode="out-in"
+      enter-active-class="animated fadeIn"
+      leave-active-class="animated fadeOut"
+    >
+      <router-view />
+   </transition>
   </div>
 </template>
 
