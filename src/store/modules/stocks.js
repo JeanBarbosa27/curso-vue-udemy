282abba3b5ad7bf04d09db4eae0ae3ccb115d54f
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-03-13 15:05:54 -0300

    Nova informação no item da lista

--- src/store/modules/stocks.js
+++ src/store/modules/stocks.js
@@ -4,21 +4,25 @@ export default {
     list: [
       {
         name: 'BMW',
+        hasError: false,
         price: 110.00,
         quantity: 0,
       },
       {
         name: 'Google',
+        hasError: false,
         price: 200.00,
         quantity: 0,
       },
       {
         name: 'Apple',
+        hasError: false,
         price: 250.00,
         quantity: 0,
       },
       {
         name: 'Twitter',
+        hasError: false,
         price: 12.00,
         quantity: 0,
       },
