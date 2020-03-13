2f4626708591016db4c04a401c818353e389906c
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-03-13 15:04:47 -0300

    Novos métodos para comprar e atualizar erro

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,14 +2,34 @@ export default {
   addListItem(state, payload) {
     return state[payload.module].list.push(payload.item);
   },
+  
+  buyStock: (state, name) => {
+    const item = state.stocks.list.filter(item => item.name === name);
+    state.portfolio.list.push(item[0]);
+  },
 
   removeListItem(state, payload) {
     const { module, name } = payload;
     return state[module].list = state[module].list.filter(item => item.name !== name);
   },
 
-  updateBalance(state, payload) { 
-    return state.balance = payload.balance;
+  sellPortfolio(state, payload) {
+    const { name, price, newBalance } = payload;
+
+  },
+
+  updateBalance(state, balance) { 
+    return state.balance = balance;
+  },
+
+  updateItemError: (state, payload) => {
+    const { module, name, hasError } = payload;
+
+    state[module].list.map((item, index) => {
+      if(item.name === name) {
+        return state[module].list[index].hasError = hasError
+      }
+    })
   },
 
   updateQuantity(state, payload) {
