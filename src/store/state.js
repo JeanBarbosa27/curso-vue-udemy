8e3e1b9917a41786fc65ef3808a884dab2248d62
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-03-14 01:12:23 -0300

    Remoção de comentário

--- src/store/state.js
+++ src/store/state.js
@@ -34,5 +34,4 @@ export default {
       boughtQuantity: 0,
     },
   ]
-  // TODO: Na verdade as ações precisam precisam estar aqui, pois na hora de vender o valor não pode ser o que foi definido na primeira interação, mas sim flutuar na mesma proporção de compra. No portifólio, de ter só uma lista com nome, quantidade e se está ou não com erro e puxando o preço do state global.
 }
