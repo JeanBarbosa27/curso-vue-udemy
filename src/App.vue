7d5ba096c95315af807ae120e3809a6fe9dff31a
Jean Barbosa <jbarbosa@estantevirtual.com.br>
2020-03-18 13:32:55 -0300

    Preparando para visualização de rotas

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-	<h1>Stock Trader</h1>
+	<v-app>
+		<Header />
+		<v-content>
+			<v-container>
+				<router-view></router-view>
+			</v-container>
+		</v-content>
+	</v-app>
 </template>
 
 <script>
-export default {
+import Header from './components/Header';
 
+export default {
+	components: {
+		Header
+	}
 }
 </script>
 
