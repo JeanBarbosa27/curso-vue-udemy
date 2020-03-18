66fdc28eb6bb631e9a616c58d19c8dbf19950753
Jean Barbosa <jbarbosa@estantevirtual.com.br>
2020-03-18 13:32:12 -0300

    Configurando vue-router

--- src/router.js
+++ src/router.js
@@ -1,17 +1,17 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-import Home from './components/Home'
-import Portfolio from './components/portfolio/Portfolio'
-import Stocks from './components/stocks/Stocks'
+import Home from './components/Home';
+import Portfolio from './components/portfolio/Portfolio.vue';
+import Stocks from './components/stocks/Stocks.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-    mode: 'history',
-    routes: [
-        { path: '/', component: Home },
-        { path: '/portfolio', component: Portfolio },
-        { path: '/stocks', component: Stocks },
-    ]
-})
+  mode: 'history',
+  routes: [
+    { path: '/', component: Home },
+    { path: '/portfolio', component: Portfolio },
+    { path: '/stocks', component: Stocks }
+  ]
+});
