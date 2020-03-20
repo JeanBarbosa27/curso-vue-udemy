b36590df8cd579a4ed4b7c762cec2cc593f6bafa
Jean Barbosa <jbarbosa@estantevirtual.com.br>
2020-03-20 18:02:17 -0300

    Aula 305: Adicionando o módulo de portfolio  ao Vuex

---
+++ src/store/modules/portfolio.js
@@ -0,0 +1,55 @@
+export default {
+  state: {
+    funds: 10000,
+    stocks: [],
+  },
+  
+  mutations: {
+    buyStock(state, { stockId, quantity, stockPrice }) {
+      const record = state.stocks.find(element => element.id === stockId);
+      if(record) {
+        record.quantity += quantity
+      } else {
+        state.stocks.push({
+          id: stockId,
+          quantity
+        })
+      }
+      state.funds -= stockPrice * quantity;
+    },
+
+    sellStock(state, { stockId, quantity, stockPrice }) {
+      const record = state.stocks.find(element => element.id === stockId);
+      if(record.quantity > quantity) {
+        record.quantity -= quantity
+      } else {
+        state.stocks.splice(state.stocks.indexOf(record), 1);
+      }
+      state.funds += stockPrice * quantity
+    }
+  },
+
+  actions: {
+    sellStock({ commit }, order) {
+      commit('sellStock', order);
+    }
+  },
+
+  getters: {
+    stockPortfolio (state, getters) {
+      return state.stocks.map(stock => {
+        const record = getters.stocks.find(element => element.id === stock.id);
+        return {
+          id: stock.id,
+          name: record.name,
+          quantity: stock.quantity,
+          price: record.price,
+        } 
+      })
+    },
+
+    funds (state) {
+      return state.funds;
+    }
+  }
+}
